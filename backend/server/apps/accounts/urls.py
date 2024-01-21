d70a388a823dfaa80962d963d26a7445a0aa93d0
arina22222 <arinacaraus@gmail.com>
2024-01-21 11:17:48 +0200

    add backend authentication

---
+++ backend/server/apps/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import re_path, include 
+
+accounts_urlpatterns = [
+    re_path(r'^api/v1/', include('djoser.urls')), 
+    re_path(r'^api/v1/', include('djoser.urls.authtoken')),
+
+]
--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from apps.accounts.urls import accounts_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1', include(accounts_urlpatterns)),
 ]
+
+urlpatterns += accounts_urlpatterns
+
